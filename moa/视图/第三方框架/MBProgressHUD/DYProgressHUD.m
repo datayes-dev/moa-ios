ffa894333bdb99e59e03b18a45f4d2ff32a1d555
yun.shu <yun.shu@datayes.com>
2016-09-13 11:16:17 +0800

    更新toast文件

--- moa/视图/第三方框架/MBProgressHUD/DYProgressHUD.m
+++ moa/视图/第三方框架/MBProgressHUD/DYProgressHUD.m
@@ -10,7 +10,19 @@
 #import "MBProgressHUD.h"
 #import "DYAppearance.h"
 
-#define KHideDelayTime 30
+#define kTipsViewHeight 36
+#define kHideDelayTime 30
+#define kDurationTime 1.0
+#define kIndicatorViewAlpha 0.6
+#define kToastViewAlpha 0.9
+
+#define DY_TEXTSIZE(text, font) [text length] > 0 ? [text \
+sizeWithAttributes:@{NSFontAttributeName:font}] : CGSizeZero;
+
+static const CGFloat DYToastPadding = 10.0;
+static const NSString * DYToastDefaultPosition = @"bottom";
+static const NSString * DYToastCenterPosition = @"center";
+
 @interface MBProgressHUD (HideGesture1)
 
 - (void)addGestureToHideProgressHUD;
@@ -44,7 +56,7 @@
 @end
 
 @implementation DYProgressHUD
-
+#pragma mark - 单例
 + (DYProgressHUD *)shareInstance
 {
     static DYProgressHUD *progressHUD;
@@ -55,16 +67,32 @@
     return progressHUD;
 }
 
+#pragma mark - 仅文字提示
+// 仅文字提示
 + (void)showProgressHUDInView:(UIView *)view message:(NSString *)message
+{
+    [[DYProgressHUD shareInstance]showProgressHUDInView:view message:message yOffset:0];
+}
+
++ (void)showProgressHUDInView:(UIView *)view message:(NSString *)message yOffset:(CGFloat)yOffset
+{
+    [[DYProgressHUD shareInstance]showProgressHUDInView:view message:message yOffset:yOffset];
+}
+
+- (void)showProgressHUDInView:(UIView *)view message:(NSString *)message yOffset:(CGFloat)yOffset
 {
     MBProgressHUD *hud = [MBProgressHUD showHUDAddedTo:view animated:YES];
     hud.mode = MBProgressHUDModeText;
     hud.removeFromSuperViewOnHide = YES;
     hud.detailsLabelText = message;
-    hud.color = DYAppearanceColor(@"H13", 0.5);
+    hud.color = DYAppearanceColor(@"H13", kIndicatorViewAlpha);
     hud.margin = 10.0f;
-    hud.detailsLabelFont = [UIFont boldSystemFontOfSize:16.0f];
-    hud.yOffset = -50.f;
+    hud.detailsLabelFont = DYAppearanceBoldFont(@"T5");
+    if (!yOffset)
+        hud.yOffset = -50.f;
+    else
+        hud.yOffset = yOffset;
+    
     hud.removeFromSuperViewOnHide = YES;
     [hud addGestureToHideProgressHUD];
 
@@ -85,13 +113,8 @@
     [hud hide:YES afterDelay:time];
 }
 
-
-
-+ (void)showWaitingIndicatorInView:(UIView *)view
-{
-    [[DYProgressHUD shareInstance]showWaitingIndicatorInView:view withHideDelayTime:KHideDelayTime];
-}
-
+#pragma mark - 自定义图片(图上文字下)
+// 显示对勾图片
 + (void)showRightIndicatorInView:(UIView *)view message:(NSString *)message
 {
     [[DYProgressHUD shareInstance]showRightIndicatorInView:view message:message];
@@ -99,9 +122,10 @@
 
 - (void)showRightIndicatorInView:(UIView *)view message:(NSString *)message
 {
-    [self showCustomIndicatorInView:view image:[UIImage imageNamed:@"successTip"] message:message];
+    [self showCustomIndicatorInView:view image:[UIImage imageNamed:@"successTip"] message:message durationTime:0 yOffset:0];
 }
 
+// 显示叉号图片
 + (void)showErrorIndicatorInView:(UIView *)view message:(NSString *)message
 {
     [[DYProgressHUD shareInstance]showErrorIndicatorInView:view message:message];
@@ -109,12 +133,47 @@
 
 - (void)showErrorIndicatorInView:(UIView *)view message:(NSString *)message
 {
-    [self showCustomIndicatorInView:view image:[UIImage imageNamed:@"error"] message:message];
+    [self showCustomIndicatorInView:view image:[UIImage imageNamed:@"error"] message:message durationTime:0 yOffset:0];
+}
+
+// 自定义图片
++ (void)showCustomIndicatorInView:(UIView *)view
+                            image:(UIImage *)image
+                          message:(NSString *)message
+{
+    [[DYProgressHUD shareInstance]showCustomIndicatorInView:view image:image message:message durationTime:0 yOffset:0];
+}
+
++ (void)showCustomIndicatorInView:(UIView *)view
+                             image:(UIImage *)image
+                           message:(NSString *)message
+                      durationTime:(double)durationTime
+{
+    [[DYProgressHUD shareInstance]showCustomIndicatorInView:view image:image message:message durationTime:durationTime yOffset:0];
+}
+
++ (void)showCustomIndicatorInView:(UIView *)view
+                            image:(UIImage *)image
+                          message:(NSString *)message
+                          yOffset:(CGFloat)yOffset
+{
+    [[DYProgressHUD shareInstance]showCustomIndicatorInView:view image:image message:message durationTime:0 yOffset:yOffset];
+}
+
++ (void)showCustomIndicatorInView:(UIView *)view
+                            image:(UIImage *)image
+                          message:(NSString *)message
+                     durationTime:(double)durationTime
+                          yOffset:(CGFloat)yOffset
+{
+    [[DYProgressHUD shareInstance]showCustomIndicatorInView:view image:image message:message durationTime:durationTime yOffset:yOffset];
 }
 
 - (void)showCustomIndicatorInView:(UIView *)view
                             image:(UIImage *)image
                           message:(NSString *)message
+                     durationTime:(double)durationTime
+                          yOffset:(CGFloat)yOffset
 {
     [self hideWaiting];
     MBProgressHUD *hud = [MBProgressHUD showHUDAddedTo:view animated:YES];
@@ -122,42 +181,52 @@
     hud.removeFromSuperViewOnHide = YES;
     hud.customView = [[UIImageView alloc] initWithImage:image];
     hud.detailsLabelText = message;
-    hud.color = [UIColor colorWithRed:0.0f green:0.0f blue:0.0f alpha:0.7];
-    hud.detailsLabelFont = [UIFont boldSystemFontOfSize:16.0f];
+    hud.yOffset = yOffset;
+    hud.color = DYAppearanceColor(@"H13", kIndicatorViewAlpha);
+    hud.detailsLabelFont = DYAppearanceBoldFont(@"T5");
     hud.removeFromSuperViewOnHide = YES;
     [hud addGestureToHideProgressHUD];
     
-    NSTimeInterval time = 0.2*message.length;
-    time = 0.5;
-    [hud hide:YES afterDelay:time];
+    if (!durationTime) {
+        durationTime = kDurationTime;
+    }
+    [hud hide:YES afterDelay:durationTime];
 }
 
 
+#pragma mark - 显示菊花（类方法）
+// 显示菊花
++ (void)showWaitingIndicatorInView:(UIView *)view
+{
+    [[DYProgressHUD shareInstance]showWaitingIndicatorInView:view withHideDelayTime:kHideDelayTime];
+}
 
-
+// 显示菊花，并在指定时间内关闭
 + (void)showWaitingIndicatorInView:(UIView *)view withHideDelayTime:(NSTimeInterval)delayTime
 {
     [[DYProgressHUD shareInstance]showWaitingIndicatorInView:view withHideDelayTime:delayTime];
 }
 
+// 显示菊花并且可以点击消失
 + (void)showWaitingIndicatorCanTouchInView:(UIView *)view
 {
     [[DYProgressHUD shareInstance]showWaitingCanTouchInView:view];
 }
+
+// 显示菊花并且不能点击消失
 + (void)showWaitingNotTouchInView:(UIView *)view
 {
     [[DYProgressHUD shareInstance]showWaitingNotTouchInView:view];
 }
 
-
+// 隐藏菊花
 + (void)hideWaiting
 {
     [[DYProgressHUD shareInstance]hideWaiting];
 }
 
 
-
-
+#pragma mark - 显示菊花（对象方法）
 - (id)init
 {
    self =  [super init];
@@ -174,7 +243,7 @@
     hud.mode  = MBProgressHUDModeIndeterminate;
     hud.removeFromSuperViewOnHide = YES;
     hud.yOffset = -50.0f;
-    hud.color =DYAppearanceColor(@"H13", 0.5);
+    hud.color =DYAppearanceColor(@"H13", kIndicatorViewAlpha);
     hud.userInteractionEnabled = YES;
     [hud addGestureToHideProgressHUD];
     [_array addObject:hud];
@@ -187,7 +256,7 @@
     hud.mode  = MBProgressHUDModeIndeterminate;
     hud.removeFromSuperViewOnHide = YES;
     hud.yOffset = -50.0f;
-    hud.color =DYAppearanceColor(@"H13", 0.5);
+    hud.color =DYAppearanceColor(@"H13", kIndicatorViewAlpha);
     hud.userInteractionEnabled = YES;
     [hud addGestureToHideProgressHUD];
     [_array addObject:hud];
@@ -202,7 +271,7 @@
     hud.mode  = MBProgressHUDModeIndeterminate;
     hud.removeFromSuperViewOnHide = YES;
     hud.yOffset = -50.0f;
-    hud.color =DYAppearanceColor(@"H13", 0.5);
+    hud.color =DYAppearanceColor(@"H13", kIndicatorViewAlpha);
     hud.userInteractionEnabled = YES;
     [hud addGestureToHideProgressHUD];
     [_array addObject:hud];
@@ -215,12 +284,12 @@
     hud.mode  = MBProgressHUDModeIndeterminate;
     hud.removeFromSuperViewOnHide = YES;
     hud.yOffset = -50.0f;
-    hud.color =DYAppearanceColor(@"H13", 0.5);
+    hud.color =DYAppearanceColor(@"H13", kIndicatorViewAlpha);
     hud.userInteractionEnabled = NO;
     [hud addGestureToHideProgressHUD];
     [_array addObject:hud];
     
-    [self performSelector:@selector(hideWaiting) withObject:self afterDelay:KHideDelayTime];
+    [self performSelector:@selector(hideWaiting) withObject:self afterDelay:kHideDelayTime];
 }
 
 - (void)hideWaiting
@@ -232,6 +301,264 @@
     [_array removeAllObjects];
 }
 
+#pragma mark - 仅显示提示文字
+// 仅显示提示文字
++ (void)showToastInView:(UIView *)view message:(NSString *)message
+{
+    UIView *toast = [[DYProgressHUD shareInstance] toastInView:view image:nil message:message toastBackGroundColor:nil];
+    [[DYProgressHUD shareInstance] showToast:toast onView:view alpha:0 duration:0 position:DYToastCenterPosition];
+}
+
++ (void)showToastInView:(UIView *)view
+                message:(NSString *)message
+   toastBackGroundColor:(UIColor *)backgroundColor
+{
+    UIView *toast = [[DYProgressHUD shareInstance] toastInView:view image:nil message:message toastBackGroundColor:backgroundColor];
+    [[DYProgressHUD shareInstance] showToast:toast onView:view alpha:0 duration:0 position:DYToastCenterPosition];
+}
+
++ (void)showToastInView:(UIView *)view
+                message:(NSString *)message
+               position:(id)position
+{
+    UIView *toast = [[DYProgressHUD shareInstance] toastInView:view image:nil message:message toastBackGroundColor:nil];
+    [[DYProgressHUD shareInstance] showToast:toast onView:view alpha:0 duration:0 position:position];
+}
+
++ (void)showToastInView:(UIView *)view
+                message:(NSString *)message
+           durationTime:(CGFloat)durationTime
+{
+    UIView *toast = [[DYProgressHUD shareInstance] toastInView:view image:nil message:message toastBackGroundColor:nil];
+    [[DYProgressHUD shareInstance] showToast:toast onView:view alpha:0 duration:durationTime position:DYToastCenterPosition];
+}
+
++ (void)showToastInView:(UIView *)view
+                message:(NSString *)message
+                  alpha:(CGFloat)alpha
+{
+    UIView *toast = [[DYProgressHUD shareInstance] toastInView:view image:nil message:message toastBackGroundColor:nil];
+    [[DYProgressHUD shareInstance] showToast:toast onView:view alpha:alpha duration:0 position:DYToastCenterPosition];
+}
+
++ (void)showToastInView:(UIView *)view
+                message:(NSString *)message
+           durationTime:(CGFloat)durationTime
+               position:(id)position
+{
+    UIView *toast = [[DYProgressHUD shareInstance] toastInView:view image:nil message:message toastBackGroundColor:nil];
+    [[DYProgressHUD shareInstance] showToast:toast onView:view alpha:0 duration:durationTime position:position];
+}
+
++ (void)showToastInView:(UIView *)view
+                message:(NSString *)message
+                  alpha:(CGFloat)alpha
+           durationTime:(CGFloat)durationTime
+               position:(id)position
+        backgroundColor:(UIColor *)backgroundColor
+{
+    UIView *toast = [[DYProgressHUD shareInstance] toastInView:view image:nil message:message toastBackGroundColor:backgroundColor];
+    [[DYProgressHUD shareInstance] showToast:toast onView:view alpha:alpha duration:durationTime position:position];
+}
+
+#pragma mark - 自定义图片（图左文字右）
+// 自定义图片 （图左文字右）
++ (void)showToastInView:(UIView *)view
+                  image:(UIImage *)image
+                message:(NSString *)message
+{
+    UIView *toast = [[DYProgressHUD shareInstance] toastInView:view image:image message:message toastBackGroundColor:nil];
+    [[DYProgressHUD shareInstance] showToast:toast onView:view alpha:0 duration:0 position:DYToastCenterPosition];
+}
+
++ (void)showToastInView:(UIView *)view
+                  image:(UIImage *)image
+                message:(NSString *)message
+               position:(id)position
+{
+    UIView *toast = [[DYProgressHUD shareInstance] toastInView:view image:image message:message toastBackGroundColor:nil];
+    [[DYProgressHUD shareInstance] showToast:toast onView:view alpha:0 duration:0 position:position];
+}
+
++ (void)showToastInView:(UIView *)view
+                  image:(UIImage *)image
+                message:(NSString *)message
+           durationTime:(CGFloat)durationTime
+{
+    UIView *toast = [[DYProgressHUD shareInstance] toastInView:view image:image message:message toastBackGroundColor:nil];
+    [[DYProgressHUD shareInstance] showToast:toast onView:view alpha:0 duration:durationTime position:DYToastCenterPosition];
+}
+
++ (void)showToastInView:(UIView *)view
+                  image:(UIImage *)image
+                message:(NSString *)message
+   toastBackGroundColor:(UIColor *)backgroundColor
+         toastViewAlpha:(CGFloat)alpha
+{
+    UIView *toast = [[DYProgressHUD shareInstance] toastInView:view image:image message:message toastBackGroundColor:backgroundColor];
+    [[DYProgressHUD shareInstance] showToast:toast onView:view alpha:alpha duration:0 position:DYToastCenterPosition];
+}
+
++ (void)showToastInView:(UIView *)view
+                  image:(UIImage *)image
+                message:(NSString *)message
+   toastBackGroundColor:(UIColor *)backgroundColor
+           durationTime:(CGFloat)durationTime
+               position:(id)position
+{
+    UIView *toast = [[DYProgressHUD shareInstance]toastInView:view image:image message:message toastBackGroundColor:backgroundColor];
+    [[DYProgressHUD shareInstance]showToast:toast onView:view alpha:0 duration:durationTime position:position];
+}
+
++ (void)showToastInView:(UIView *)view
+                  image:(UIImage *)image
+                message:(NSString *)message
+   toastBackGroundColor:(UIColor *)backgroundColor
+         toastViewAlpha:(CGFloat)alpha
+           durationTime:(CGFloat)durationTime
+               position:(id)position
+{
+    UIView *toast = [[DYProgressHUD shareInstance]toastInView:view image:image message:message toastBackGroundColor:backgroundColor];
+    [[DYProgressHUD shareInstance]showToast:toast onView:view alpha:alpha duration:durationTime position:position];
+}
+
+#pragma mark - Private Methods
+// 创建toast
+- (UIView *)toastInView:(UIView *)view
+                  image:(UIImage *)image
+                message:(NSString *)message
+   toastBackGroundColor:(UIColor *)backgroundColor
+{
+    if (image == nil && message == nil) {
+        return nil;
+    }
+    
+    UIImageView *imageView = nil;
+    UILabel *messageLabel = nil;
+    
+    // toastView
+    UIView *toastView = [[UIView alloc] init];
+    if (!backgroundColor) {
+        toastView.backgroundColor = DYAppearanceColor(@"B4",kToastViewAlpha);
+    }
+    toastView.layer.cornerRadius = 3;
+    
+    // imageView
+    if (image != nil) {
+        imageView = [[UIImageView alloc] initWithImage:image];
+        imageView.contentMode = UIViewContentModeScaleAspectFit;
+        imageView.frame = CGRectMake(DYToastPadding, DYToastPadding, image.size.width, image.size.height);
+    }
+    
+    CGFloat imageWidth, imageHeight, imageLeft;
+    if (imageView != nil) {
+        imageWidth = imageView.bounds.size.width;
+        imageHeight = imageView.bounds.size.height;
+        imageLeft = DYToastPadding;
+    }else {
+        imageWidth = imageHeight = imageLeft = 0.0;
+    }
+    
+    // messageLabel
+    if (message != nil) {
+        messageLabel = [[UILabel alloc] init];
+        messageLabel.text = message;
+        messageLabel.numberOfLines = 0;
+        messageLabel.textColor = DYAppearanceColor(@"W1",1.0);
+        messageLabel.font = DYAppearanceFont(@"T3");
+        
+        CGFloat toastMaxWidth = 0.8;
+        CGSize maxSizeMessage = CGSizeMake((view.bounds.size.width * toastMaxWidth) - imageWidth, view.bounds.size.height * toastMaxWidth);
+        NSDictionary *attributes = @{NSFontAttributeName:messageLabel.font};
+        CGRect expectedRectMessage = [message boundingRectWithSize:maxSizeMessage options:NSStringDrawingUsesLineFragmentOrigin attributes:attributes context:nil];
+        messageLabel.frame = CGRectMake(0.0, 0.0, expectedRectMessage.size.width, expectedRectMessage.size.height);
+    }
+    
+    CGFloat labelWidth, labelHeight,labelLeft, labelTop;
+    if (messageLabel != nil) {
+        labelWidth = messageLabel.bounds.size.width;
+        labelHeight = messageLabel.bounds.size.height;
+        labelLeft = imageLeft + imageWidth + DYToastPadding;
+        labelTop = DYToastPadding;
+    }else {
+        labelWidth = labelHeight = labelLeft = labelTop = 0.0;
+    }
+    
+    // toastView frame
+    CGFloat toastViewW = MAX((imageWidth + (DYToastPadding * 2)),labelLeft + labelWidth + DYToastPadding);
+    CGFloat toastViewH = MAX((labelHeight + DYToastPadding + labelTop), (imageHeight + (DYToastPadding * 2)));
+    toastView.frame = CGRectMake(0, 0, toastViewW, toastViewH);
+    
+    // messageLabel frame
+    if (messageLabel != nil) {
+        messageLabel.frame = CGRectMake(labelLeft, labelTop, labelWidth, labelHeight);
+        [toastView addSubview:messageLabel];
+    }
+    
+    // imageView frame
+    if (imageView != nil) {
+        CGRect imageViewF = imageView.frame;
+        imageViewF.origin.y = (toastView.frame.size.height - imageHeight) * 0.5;
+        imageView.frame = imageViewF;
+        [toastView addSubview:imageView];
+    }
+    
+    return toastView;
+}
+
+// 显示toast
+- (void)showToast:(UIView *)toast onView:(UIView *)superView alpha:(CGFloat)alpha duration:(CGFloat)durationTime position:(id)point {
+    toast.center = [self centerPointForPosition:point withToast:toast withView:superView];
+    [superView addSubview:toast];
+    
+    if (!alpha) {
+        alpha = 1.0;
+    }
+    toast.alpha = alpha;
+    
+    // animation
+    if (!durationTime) {
+        durationTime = kDurationTime;
+    }
+    
+    [UIView animateWithDuration:durationTime
+                          delay:0.0
+                        options:UIViewAnimationOptionCurveEaseOut
+                     animations:^{
+                         toast.alpha = 1.0;
+                     } completion:^(BOOL finished) {
+                         [UIView animateWithDuration:0.2
+                                               delay:durationTime
+                                             options:UIViewAnimationOptionCurveEaseIn
+                                          animations:^{
+                                              toast.alpha = 0.0;
+                                          } completion:^(BOOL finished) {
+                                              [toast removeFromSuperview];
+                                          }];
+                     }];
+}
+
+// toast frame
+- (CGPoint)centerPointForPosition:(id)point withToast:(UIView *)toast withView:(UIView *)view {
+    if([point isKindOfClass:[NSString class]]) {
+        
+        if([point caseInsensitiveCompare:@"top"] == NSOrderedSame) {
+            return CGPointMake(view.bounds.size.width/2, (toast.frame.size.height / 2) + DYToastPadding);
+            
+        } else if([point caseInsensitiveCompare:@"bottom"] == NSOrderedSame) {
+            return CGPointMake(view.bounds.size.width/2, (view.bounds.size.height - (toast.frame.size.height / 2)) - DYToastPadding);
+            
+        } else if([point caseInsensitiveCompare:@"center"] == NSOrderedSame) {
+            return CGPointMake(view.bounds.size.width / 2, view.bounds.size.height / 2);
+            
+        }
+    } else if ([point isKindOfClass:[NSValue class]]) {
+        return [point CGPointValue];
+    }
+    
+    NSLog(@"Warning: Invalid position for toast.");
+    return [self centerPointForPosition:DYToastDefaultPosition withToast:toast withView:view];
+}
 
 
 @end
