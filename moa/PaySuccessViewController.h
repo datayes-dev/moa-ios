39c9a1ef70cd801544f198a6ec2622d9e35e5318
xiaojun.song <xiaojun.song@datayes.com>
2016-09-14 17:37:28 +0800

    mao-0001 支付成功页xib

---
+++ moa/PaySuccessViewController.h
@@ -0,0 +1,14 @@
+//
+//  PaySuccessViewController.h
+//  moa
+//
+//  Created by datayes on 16/9/14.
+//  Copyright © 2016年 datayes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "MOAViewController.h"
+
+@interface PaySuccessViewController : MOAViewController
+
+@end
