b5f02110655adfe0ebe3bb0c29451a07dd241846
鄢彭超 <pengchao.yan@datayes.com>
2016-11-01 11:23:46 +0800

    添加了我的二维码的页面

---
+++ moa/控制器/我的/MyQRCodeViewController.h
@@ -0,0 +1,14 @@
+//
+//  MyQRCodeViewController.h
+//  moa
+//
+//  Created by 鄢彭超 on 2016/10/31.
+//  Copyright © 2016年 datayes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "MOAViewController.h"
+
+@interface MyQRCodeViewController : MOAViewController
+
+@end
